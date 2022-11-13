b2bb4a7e67af4ff1b1997c02afc462d8ae72c52b
aleshinme <115113394+aleshinme@users.noreply.github.com>
2022-11-13 17:41:11 +0300

    Update task_1.py

--- лабораторная 5/task_1.py
+++ лабораторная 5/task_1.py
@@ -2,7 +2,7 @@
 from pprint import pprint
 copy = []  # Создание пустого списка
 for i in range(0, 16):  # Задаем отрезок чисел
-    dictionary = {'bin': bin(i), 'dec': i, 'hex': hex(i), 'oct': oct(i)}  # Система исчесления - значение в этой системе
+    dictionary = {'bin': bin(i), 'dec': i, 'hex': hex(i), 'oct': oct(i)}  # Система исчисления - значение в этой системе
     copy.append(dictionary)  # Позволяет склеить все словари в список
 
 pprint(copy)
